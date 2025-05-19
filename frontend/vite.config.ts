b3854f5ace8a8f0d718de80c6483c752f81d80f5
JoaquimAlt <joaquimferreira1818@gmail.com>
2025-05-19 00:41:04 -0300

    preparacao para deploy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,19 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5555'
+export default defineConfig(({ mode }) => {
+  // Carrega as variáveis de ambiente do arquivo .env
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+        },
       },
     },
-  },
+  }
 })
