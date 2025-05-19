322d0cfb488e5589f45456866ae63b0f89bcd30a
JoaquimAlt <joaquimferreira1818@gmail.com>
2025-05-18 23:02:07 -0300

    fix: inde.js

--- backend/index.js
+++ backend/index.js
@@ -3,27 +3,17 @@ import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import rateRoutes from "./routes/rate.route.js"
 import authRoutes from "./routes/user.route.js"
-import path from "path"
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5555;
 
-const __dirname = path.resolve();
-
 app.use(express.json()); 
 
 app.use("/api/rates", rateRoutes);
 
 app.use("/api/users", authRoutes);
-
-if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
-	app.get("/{*splat}", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-	});
-}
 
 app.listen(PORT, () => {
     connectDB();
