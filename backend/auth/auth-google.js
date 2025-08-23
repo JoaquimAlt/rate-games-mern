54129eb60e48cd60555862345cdfd37d00bd4fc1
JoaquimAlt <joaquimferreira1818@gmail.com>
2025-08-23 12:19:21 -0300

    fix: color footer, url google callback

--- backend/auth/auth-google.js
+++ backend/auth/auth-google.js
@@ -7,7 +7,7 @@ const { generateToken } = require('../controllers/user.controller');
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:5555/auth/google/callback"
+    callbackURL: `${process.env.SERVER_URL}/auth/google/callback`
 }, async (accessToken, refreshToken, profile, done) => {
     try {
         const email = profile.emails[0].value;
