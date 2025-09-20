fc56ad001d2082da58e1223289b2d98840ba2979
JoaquimFS <63430363+JoaquimAlt@users.noreply.github.com>
2025-09-20 19:53:37 -0300

    fix: cursor pointer

--- frontend/src/components/GameCard.tsx
+++ frontend/src/components/GameCard.tsx
@@ -34,7 +34,7 @@ const GameCard = ({ game }: Props) => {
                 gap={4}
                 onClick={() => handleSelect(game.id)}
                 borderRadius={5}
-                cursor="pointer
+                cursor="pointer"
             >
                 <VStack alignItems={"flex-start"} p={5}>
                     <Text
